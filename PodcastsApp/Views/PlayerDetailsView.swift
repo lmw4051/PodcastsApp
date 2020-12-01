00541099eb69d8595fc6d9c8231233953452aaab
David <dvdlee41@gmail.com>
2020-12-01 11:45:35 +0800

    . Adding handleCurrentTimeSliderChange function . Adding handleFastForward & handleRewind function . Adding handleVolumeChange function

--- PodcastsApp/Views/PlayerDetailsView.swift
+++ PodcastsApp/Views/PlayerDetailsView.swift
@@ -57,10 +57,32 @@ class PlayerDetailsView: UIView {
     }
   }
   
-  // MARK: - IBActions
+  // MARK: - IBAction Methods
   @IBAction func handleDismiss(_ sender: Any) {
     self.removeFromSuperview()
   }
+  
+  @IBAction func handleCurrentTimeSliderChange(_ sender: Any) {
+    let percentage = currentTimeSlider.value
+    guard let duration = player.currentItem?.duration else { return }
+    let durationInSeconds = CMTimeGetSeconds(duration)
+    let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+    let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+    player.seek(to: seekTime)
+  }
+  
+  @IBAction func handleFastForward(_ sender: Any) {
+    seekToCurrentTime(delta: 15)
+  }
+  
+  @IBAction func handleRewind(_ sender: Any) {
+    seekToCurrentTime(delta: -15)
+  }
+  
+  @IBAction func handleVolumeChange(_ sender: UISlider) {
+    player.volume = sender.value
+  }
+  
   
   // MARK: - View Life Cycle
   override func awakeFromNib() {
@@ -115,6 +137,12 @@ class PlayerDetailsView: UIView {
     let percentage = currentTimeSeconds / durationSeconds
     
     self.currentTimeSlider.value = Float(percentage)
+  }
+  
+  fileprivate func seekToCurrentTime(delta: Int64) {
+    let fifteenSeconds = CMTimeMake(value: delta, timescale: 1)
+    let seekTime = CMTimeAdd(player.currentTime(), fifteenSeconds)
+    player.seek(to: seekTime)
   }
   
   // MARK: - Selector Methods
